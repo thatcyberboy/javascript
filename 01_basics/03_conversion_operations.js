4cea958290be18ceece40a5c3be59eaf6c0328ff
Akash Vishwakarma <90363008+thatcyberboy@users.noreply.github.com>
2024-02-04 14:11:50 +0000

    conversion of data types

--- 01_basics/03_conversion_operations.js
+++ 01_basics/03_conversion_operations.js
@@ -1,28 +1,31 @@
-// let score = null
-// let age = true
-// let marks = "123abc"
+let score = null
+let age = true
+let marks = "123abc"
 
-// console.table([typeof score, typeof age, typeof marks])
+console.table([typeof score, typeof age, typeof marks])
 
-// let new_score = Number(score)
-// let new_age = Number(age)
-// let new_marks = Number(marks)
+let new_score = Number(score)
+let new_age = Number(age)
+let new_marks = Number(marks)
 
-// console.table([typeof new_score, typeof new_age, typeof new_marks])
-// console.table([new_score, new_age, new_marks])
+console.table([typeof new_score, typeof new_age, typeof new_marks])
+console.table([new_score, new_age, new_marks])
 
-// "33" => 33
-// "334agfd" => NaN
-// true => 0
-// false => 0
-// null => 0
+/*
+
+"33" => 33
+"334agfd" => NaN
+true => 0
+false => 0
+null => 0
+
+*/
 
 
-
-// let is_logged_in = ""
-// is_logged_in = Boolean(is_logged_in)
-// console.log(typeof is_logged_in)
-// console.log(is_logged_in);  
+let is_logged_in = ""
+is_logged_in = Boolean(is_logged_in)
+console.log(typeof is_logged_in)
+console.log(is_logged_in);  
 
 /*
 
